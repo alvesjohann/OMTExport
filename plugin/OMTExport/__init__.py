771aa72785b7fd1d428567ceba08da3f6ff1f271
Johann Alves <24369119+alvesjohann@users.noreply.github.com>
2025-07-16 18:16:32 -0300

    Refactor functions to separate module
    Moved panel edge banding update functions from __init__.py to a new functions.py module for better code organization. Updated import in __init__.py and incremented version in blender_manifest.toml.

--- plugin/OMTExport/__init__.py
+++ plugin/OMTExport/__init__.py
@@ -15,56 +15,7 @@ from bpy.props import (
     StringProperty
 )
 
-########################################### Funcions ###########################################
-
-def UPDATED_PANEL_EDGE_WHITE_MATERIAL(self, context):
-    SCENE = context.scene
-    OMT_TOOL = SCENE.OMT_Export_tool
-        
-    if OMT_TOOL.PANEL_EDGE_BANDING_WHITE_MATERIAL:
-        OMT_TOOL.PANEL_EDGE_BANDING_WOOD_MATERIAL = False
-        OMT_TOOL.PANEL_EDGE_BANDING_BLACK_MATERIAL = False
-
-def UPDATED_PANEL_EDGE_WOOD_MATERIAL(self, context):
-    SCENE = context.scene
-    OMT_TOOL = SCENE.OMT_Export_tool
-        
-    if OMT_TOOL.PANEL_EDGE_BANDING_WOOD_MATERIAL:
-        OMT_TOOL.PANEL_EDGE_BANDING_WHITE_MATERIAL = False
-        OMT_TOOL.PANEL_EDGE_BANDING_BLACK_MATERIAL = False
-
-def UPDATED_PANEL_EDGE_BLACK_MATERIAL(self, context):
-    SCENE = context.scene
-    OMT_TOOL = SCENE.OMT_Export_tool
-        
-    if OMT_TOOL.PANEL_EDGE_BANDING_BLACK_MATERIAL:
-        OMT_TOOL.PANEL_EDGE_BANDING_WHITE_MATERIAL = False
-        OMT_TOOL.PANEL_EDGE_BANDING_WOOD_MATERIAL = False
-    
-def UPDATED_PANEL_EDGE_BANDING_22MM(self, context):
-    SCENE = context.scene
-    OMT_TOOL = SCENE.OMT_Export_tool
-        
-    if OMT_TOOL.PANEL_EDGE_BANDING_22MM:
-        OMT_TOOL.PANEL_EDGE_BANDING_45MM = False
-        OMT_TOOL.PANEL_EDGE_BANDING_100MM = False
-    
-def UPDATED_PANEL_EDGE_BANDING_45MM(self, context):
-    SCENE = context.scene
-    OMT_TOOL = SCENE.OMT_Export_tool
-        
-    if OMT_TOOL.PANEL_EDGE_BANDING_45MM:
-        OMT_TOOL.PANEL_EDGE_BANDING_22MM = False
-        OMT_TOOL.PANEL_EDGE_BANDING_100MM = False
-    
-def UPDATED_PANEL_EDGE_BANDING_100MM(self, context):
-    SCENE = context.scene
-    OMT_TOOL = SCENE.OMT_Export_tool
-        
-    if OMT_TOOL.PANEL_EDGE_BANDING_100MM:
-        OMT_TOOL.PANEL_EDGE_BANDING_22MM = False
-        OMT_TOOL.PANEL_EDGE_BANDING_45MM = False
-
+from .functions import *
 
 ###########################################  Properties ###########################################
 
